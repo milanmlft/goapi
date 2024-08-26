3d6945bcc04b7619077dc012e69f3049cb604f9d
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-08-26 15:00:42 +0100

    Add request error handlers

--- api/api.go
+++ api/api.go
@@ -1,5 +1,10 @@
 package api
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type IceCreamParams struct {
 	Username string
 }
@@ -15,3 +20,26 @@ type Error struct {
 	Message string
 	Code    int
 }
+
+// Function to write errors to the HTTP response writer
+func writeError(w http.ResponseWriter, message string, code int) {
+	resp := Error{
+		Code:    code,
+		Message: message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(resp)
+}
+
+var (
+	// To return specific errors in response, useful for user
+	RequestErrorHandler = func(w http.ResponseWriter, err error) {
+		writeError(w, err.Error(), http.StatusBadRequest)
+	}
+	// To return a generic error message, e.g. internal bugs, not useful for user
+	InternalErrorHandler = func(w http.ResponseWriter) {
+		writeError(w, "An Unexpected Error Occurred.", http.StatusInternalServerError)
+	}
+)
